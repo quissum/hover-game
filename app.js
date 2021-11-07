dce6484442ae8dd71339e56d78bee72aeb0aabdd
quissum <kingvey13@gmail.com>
2021-11-07 22:04:58 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,40 @@
+const board = document.querySelector('#board')
+const SQUARES_NUMBER = 500
+const colors = [
+  '#001219',
+  '#005f73',
+  '#94d2bd',
+  '#94d2bd',
+  '#e9d8a6',
+  '#ee9b00',
+  '#ca6702',
+  '#bb3e03',
+  '#ae2012',
+  '#9b2226',
+]
+
+for (let i = 0; i < SQUARES_NUMBER; i++) {
+  const square = document.createElement('div')
+  square.classList.add('square')
+
+  square.addEventListener('mouseover', () => setColor(square))
+  square.addEventListener('mouseleave', () => removeColor(square))
+
+  board.append(square)
+}
+
+function setColor(element) {
+  const color = getRandomColor()
+  element.style.backgroundColor = color
+  element.style.boxShadow = `0 0 2px ${color},0 0 10px ${color}`
+}
+
+function removeColor(element) {
+  element.style.backgroundColor = '#1d1d1d'
+  element.style.boxShadow = `0 0 2px #000`
+}
+
+function getRandomColor() {
+  const index = Math.floor(Math.random() * colors.length)
+  return colors[index]
+}
